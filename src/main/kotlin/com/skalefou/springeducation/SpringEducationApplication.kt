a37e17aa0122f13263bd0dbbfe2b0067213999b8
Skalefou <skamsagar@gmail.com>
2026-01-28 14:15:04 +0100

    chore(init): Initialisation project

---
+++ src/main/kotlin/com/skalefou/springeducation/SpringEducationApplication.kt
@@ -0,0 +1,11 @@
+package com.skalefou.springeducation
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringEducationApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringEducationApplication>(*args)
+}
---
+++ src/test/kotlin/com/skalefou/springeducation/SpringEducationApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.skalefou.springeducation
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringEducationApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
